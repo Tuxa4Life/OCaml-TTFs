d5af34605745c300ae6ba298a2dfd9ceb32c3f50
Tuxa4Life <nikoloztuxa@gmail.com>
2024-05-07 16:32:41 +0400

    Lazy Lists

--- Extras/practice.ml
+++ Extras/practice.ml
@@ -1,4 +1,13 @@
-let length l = List.fold_left (fun acc _ -> acc + 1) 0 l
-let rev l = List.fold_left (fun acc x -> x::acc) [] l
-let map f l = List.fold_right (fun x acc -> (f x)::acc) l []
-let filter pred l = List.fold_right (fun x acc -> if pred x then x::acc else acc) l []
+type 'a llist = Cons of 'a * (unit -> 'a llist)
+
+let rec lnat start = Cons (start, (fun () -> lnat (start + 1))) 
+
+let lfib () =
+  let rec aux a b = Cons(a, (fun () -> aux b (a + b))) in 
+  aux 0 1 
+
+let rec take n ll = if n = 0 then [] else match ll with 
+  Cons (x, f) -> x::take (n - 1) (f ())
+
+let rec lfilter pred ll = match ll with 
+  Cons (x, f) -> if pred x then Cons(x, (fun () -> lfilter pred (f ()))) else lfilter pred (f ())
